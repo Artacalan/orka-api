c6282d2c8962853b36dd265ddbe5b1647400b081
perr0112 <prtclement.ctc@gmail.com>
2026-06-25 15:20:51 +0200

    feat: use of erpRouter + add routes related to 'biens'

--- src/api/server.js
+++ src/api/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const biensRouter = require('./routes/biens');
+const erpRouter = require('./routes/erp')
 
 const app = express();
 app.use(express.json({ limit: '50mb' }));
@@ -9,6 +10,7 @@ app.get('/hello', (req, res) => {
     res.status(200).json({ message: 'hello world' });
 });
 
+app.use('/api', erpRouter)
 app.use('/api/biens', biensRouter);
 
 const PORT = process.env.PORT || 3000;
